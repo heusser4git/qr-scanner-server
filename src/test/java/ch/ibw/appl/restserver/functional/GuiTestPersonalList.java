e1ac60fba5506239b04711357bcfdac4990df544
Urs Heusser <urs.heusser@fhgr.ch>
2022-12-22 14:12:42 +0100

    New TestFile -> actions Test

---
+++ src/test/java/ch/ibw/appl/restserver/functional/GuiTestPersonalList.java
@@ -0,0 +1,34 @@
+package ch.ibw.appl.restserver.functional;
+
+import ch.ibw.appl.restserver.functional.shared.guitest.PersonalPo;
+import ch.ibw.appl.restserver.functional.shared.guitest.SeleniumHelper;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.Assert.assertEquals;
+
+public class GuiTestPersonalList {
+    private static WebDriver driver;
+    @BeforeClass
+    public static void setUp()
+    {
+        driver = SeleniumHelper.setUpWebDriver();
+    }
+
+    @Test
+    public void testHeaderText() {
+        WebElement header = driver.findElement(PersonalPo.headerTagSelector());
+        header.findElement(PersonalPo.aTagSelector());
+        assertEquals("QR-Scanner", header.getText());
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        if (driver != null) {
+            SeleniumHelper.tearDownWebDriver(driver);
+        }
+    }
+}
