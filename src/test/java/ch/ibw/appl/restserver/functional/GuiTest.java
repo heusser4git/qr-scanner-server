331f7b1f35f9f51ff6e6b58caaf681ca49ad9bdb
Urs Heusser <urs.heusser@fhgr.ch>
2023-01-06 20:23:59 +0100

    give test up -> back to root

--- src/test/java/ch/ibw/appl/restserver/functional/GuiTest.java
+++ src/test/java/ch/ibw/appl/restserver/functional/GuiTest.java
@@ -3,11 +3,13 @@ package ch.ibw.appl.restserver.functional;
 import ch.ibw.appl.restserver.functional.shared.guitest.PersonalPo;
 import ch.ibw.appl.restserver.functional.shared.guitest.SeleniumHelper;
 import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -16,96 +18,87 @@ public class GuiTest {
     private static WebDriver driver;
 
 
-//    @Test
-//    public void testHeaderText() {
-//        driver = SeleniumHelper.setUpWebDriver();
-//        WebElement header = driver.findElement(PersonalPo.headerTagSelector());
-//        header.findElement(PersonalPo.aTagSelector());
-//        assertEquals("QR-Scanner", header.getText());
-//    }
-//
-//    @Test
-//    public void testNavigationToAdministrationByButton(){
-//        driver = SeleniumHelper.setUpWebDriver();
-//        WebElement adminButton = driver.findElement(PersonalPo.adminButtonSelector());
-//        adminButton.click();
-//        WebElement adminTitel = driver.findElement(By.tagName("h1"));
-//
-//        assertEquals("Administration", adminTitel.getText());
-//    }
-//
-//    @Test
-//    public void testNavigationToAdministrationByPicture(){
-//        driver = SeleniumHelper.setUpWebDriver();
-//        WebElement adminPicture = driver.findElement(By.cssSelector("img[src='img/administration.png']"));
-//        adminPicture.click();
-//        WebElement adminTitel = driver.findElement(By.tagName("h1"));
-//
-//        assertEquals("Administration", adminTitel.getText());
-//    }
-//
-//    @Test
-//    public void testNavigationToScannerByButton(){
-//        driver = SeleniumHelper.setUpWebDriver();
-//        WebElement scannerButton = driver.findElement(By.cssSelector("button[id='button-scanner']"));
-//        scannerButton.click();
-//        WebElement scannerTitel = driver.findElement(By.tagName("h1"));
-//
-//        assertEquals("Scanner", scannerTitel.getText());
-//    }
-//
-//    @Test
-//    public void testNavigationToScannerByPicture(){
-//        driver = SeleniumHelper.setUpWebDriver();
-//        WebElement scannerPicture = driver.findElement(By.cssSelector("img[src='img/scanner.jpg']"));
-//        scannerPicture.click();
-//        WebElement scannerTitel = driver.findElement(By.tagName("h1"));
-//
-//        assertEquals("Scanner", scannerTitel.getText());
-//    }
+    @Test
+    public void testHeaderText() {
+        driver = SeleniumHelper.setUpWebDriver();
+        WebElement header = driver.findElement(PersonalPo.headerTagSelector());
+        header.findElement(PersonalPo.aTagSelector());
+        assertEquals("QR-Scanner", header.getText());
+    }
 
     @Test
-    public void testSearchOnList() throws InterruptedException {
+    public void testNavigationToAdministrationByButton(){
+        driver = SeleniumHelper.setUpWebDriver();
+        WebElement adminButton = driver.findElement(PersonalPo.adminButtonSelector());
+        adminButton.click();
+        WebElement adminTitel = driver.findElement(By.tagName("h1"));
+
+        assertEquals("Administration", adminTitel.getText());
+    }
+
+    @Test
+    public void testNavigationToAdministrationByPicture(){
+        driver = SeleniumHelper.setUpWebDriver();
+        WebElement adminPicture = driver.findElement(By.cssSelector("img[src='img/administration.png']"));
+        adminPicture.click();
+        WebElement adminTitel = driver.findElement(By.tagName("h1"));
+
+        assertEquals("Administration", adminTitel.getText());
+    }
+
+    @Test
+    public void testNavigationToScannerByButton(){
+        driver = SeleniumHelper.setUpWebDriver();
+        WebElement scannerButton = driver.findElement(By.cssSelector("button[id='button-scanner']"));
+        scannerButton.click();
+        WebElement scannerTitel = driver.findElement(By.tagName("h1"));
+
+        assertEquals("Scanner", scannerTitel.getText());
+    }
+
+    @Test
+    public void testNavigationToScannerByPicture(){
+        driver = SeleniumHelper.setUpWebDriver();
+        WebElement scannerPicture = driver.findElement(By.cssSelector("img[src='img/scanner.jpg']"));
+        scannerPicture.click();
+        WebElement scannerTitel = driver.findElement(By.tagName("h1"));
+
+        assertEquals("Scanner", scannerTitel.getText());
+    }
+
+    /*@Test
+    public void testSearchOnList() {
         driver = SeleniumHelper.setUpWebDriver();
         WebElement adminButton = driver.findElement(By.cssSelector("button[id='button-admin']"));
         adminButton.click();
-        Thread.sleep(1000);
         WebElement searchField = driver.findElement(PersonalPo.searchInputSelector());
         searchField.sendKeys("perko");
-        Thread.sleep(1000);
-        WebElement mainContent = driver.findElement(By.cssSelector("main[id='main-content']"));
-        WebElement table = mainContent.findElement(By.tagName("table"));
-        List<WebElement> trs = table.findElements(By.tagName("tr"));
-        List<WebElement> tds = null;
-        for (WebElement tr: trs) {
-            if(tr.getAttribute("data-row").contains("2")) {
-                tds = tr.findElements(By.tagName("td"));
-            }
-        }
-        String result = "";
-        assertEquals("anzahl", tds.size());
-        for (WebElement td : tds) {
-            assertEquals("Perko", td.getText());
-            if(td.getText().contains("Perko")) {
-                result = td.getText();
-            }
-        }
+        WebElement tdFamiliyName = driver.findElement(By.xpath("/html/body/main/div[1]/table/tbody/tr/td[2]"));
+        System.out.println(tdFamiliyName.getLocation());
+        //WebElement tdFamiliyName = driver.findElement(By.xpath("//*[@id=\"main-content\"]/div[1]/table/tbody/tr/td[2]"));
+        //WebElement tdFirstname = driver.findElement(By.xpath("//*[@id=\"main-content\"]/div[1]/table/tbody/tr/td[3]"));
+        WebElement tdBirthday = driver.findElement(By.xpath("/html/body/main/div[1]/table/tbody/tr/td[4]"));
+        WebElement tdStatus = driver.findElement(By.xpath("/html/body/main/div[1]/table/tbody/tr/td[5]"));
 
-        assertEquals("Perko", result);
+        assertEquals("Perko", tdFamiliyName.getText());
+        assertEquals("Mitja", tdFirstname.getText());
+        assertEquals("Perko", tdFamiliyName.getText());
+        assertEquals("Perko", tdFamiliyName.getText());
+        assertEquals("Perko", tdFamiliyName.getText());
     }
+    */
 
-
-//    @Test
-//    public void testAddOnButtonAndItsModalWindow() throws InterruptedException {
-//        driver = SeleniumHelper.setUpWebDriver();
-//        WebElement adminButton = driver.findElement(By.cssSelector("button[id='button-admin']"));
-//        adminButton.click();
-//        WebElement addButton = driver.findElement(PersonalPo.addItemButtonSelector());
-//        addButton.click();
-//        Thread.sleep(200);
-//        WebElement title = driver.findElement(By.xpath("/html/body/main/div[3]/div/div[1]/h3"));
-//        assertEquals("Neuen Benutzer hinzufügen", title.getAttribute("innerHTML"));
-//    }
+    @Test
+    public void testAddOnButtonAndItsModalWindow() throws InterruptedException {
+        driver = SeleniumHelper.setUpWebDriver();
+        WebElement adminButton = driver.findElement(By.cssSelector("button[id='button-admin']"));
+        adminButton.click();
+        WebElement addButton = driver.findElement(PersonalPo.addItemButtonSelector());
+        addButton.click();
+        Thread.sleep(200);
+        WebElement title = driver.findElement(By.xpath("/html/body/main/div[3]/div/div[1]/h3"));
+        assertEquals("Neuen Benutzer hinzufügen", title.getAttribute("innerHTML"));
+    }
     @AfterClass
     public static void tearDown(){
         if (driver != null) {
