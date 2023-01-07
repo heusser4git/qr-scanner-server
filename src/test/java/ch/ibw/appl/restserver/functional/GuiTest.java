62b4f0621fef4d6d38f857a85418602d34455a6e
MitjaPerko <89871456+MitjaPerko@users.noreply.github.com>
2023-01-07 10:21:13 +0100

    Change PersonalPo.java Selectors

--- src/test/java/ch/ibw/appl/restserver/functional/GuiTest.java
+++ src/test/java/ch/ibw/appl/restserver/functional/GuiTest.java
@@ -63,17 +63,17 @@ public class GuiTest {
         driver = SeleniumHelper.setUpWebDriver();
         WebElement adminButton = driver.findElement(PersonalPo.adminButtonSelector());
         adminButton.click();
-        //WebElement searchField = driver.findElement(PersonalPo.searchInputSelector());
-        //searchField.sendKeys("perko");
-        Thread.sleep(200);
-        WebElement tdFamiliyName = driver.findElement(By.xpath("/html/body/main/div[1]/table/tbody/tr[2]/td[2]"));
+        WebElement searchField = driver.findElement(PersonalPo.searchInputSelector());
+        searchField.sendKeys("perko");
+        Thread.sleep(2000);
+        String tdFamiliyName = driver.findElement(By.xpath("/html/body/main/div[1]/table/tbody/tr/td[2]")).getAttribute("innerHTML");
         //System.out.println(tdFamiliyName.getLocation());
         //WebElement tdFamiliyName = driver.findElement(By.xpath("//*[@id=\"main-content\"]/div[1]/table/tbody/tr/td[2]"));
         //WebElement tdFirstname = driver.findElement(By.xpath("//*[@id=\"main-content\"]/div[1]/table/tbody/tr/td[3]"));
         //WebElement tdBirthday = driver.findElement(By.xpath("/html/body/main/div[1]/table/tbody/tr/td[4]"));
         //WebElement tdStatus = driver.findElement(By.xpath("/html/body/main/div[1]/table/tbody/tr/td[5]"));
 
-        assertEquals("Perko", tdFamiliyName.getText());
+        assertEquals("Perko ", tdFamiliyName);
         //assertEquals("Mitja", tdFirstname.getText());
         //assertEquals("Perko", tdFamiliyName.getText());
         //assertEquals("Perko", tdFamiliyName.getText());
