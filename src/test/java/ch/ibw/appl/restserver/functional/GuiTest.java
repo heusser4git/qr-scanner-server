4b4c98a307c51ba9243e4de3d3403a366905017a
MitjaPerko <89871456+MitjaPerko@users.noreply.github.com>
2023-01-09 23:53:10 +0100

    Change Gui TEst

--- src/test/java/ch/ibw/appl/restserver/functional/GuiTest.java
+++ src/test/java/ch/ibw/appl/restserver/functional/GuiTest.java
@@ -84,17 +84,16 @@ public class GuiTest {
         //assertEquals("Perko", tdFamiliyName.getText());
         //assertEquals("Perko", tdFamiliyName.getText());
     }*/
-    /*@Test
+    @Test
     public void testsample(){
         driver = SeleniumHelper.setUpWebDriver();
         WebElement adminButton = driver.findElement(PersonalPo.adminButtonSelector());
         adminButton.click();
         WebElement searchField = driver.findElement(PersonalPo.searchInputSelector());
         searchField.sendKeys("perko");
-        WebElement td = driver.findElement(By.cssSelector("p[id='result']"));
-        //System.out.println(td.toString());
+        WebElement td = driver.findElement(By.cssSelector("p[id='vorname2']"));
         assertEquals("Mitja", td.getText());
-    }*/
+    }
 
     /*@Test
     public void testAddOnButtonAndItsModalWindow() throws InterruptedException {
